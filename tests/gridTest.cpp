845b85b8124e867b621d6c1246bb2e00b9dfd40b
Eric S Osburn <bjonesen@gmail.com>
2016-06-03 01:39:39 -0500

    First commit on Grid branch.

---
+++ tests/gridTest.cpp
@@ -0,0 +1,18 @@
+#include "includes/main.h"
+
+#include "includes/grid.h"
+
+// Make it a checkerboard
+void mapFunc( uint32_t * cell, uint32_t i, uint32_t j ) {
+    (*cell) = ( i + j ) % 2;
+}
+
+void gridTest( ) {
+
+    CodeMonkey::DataStructs::IGrid g( 5, 5 );
+
+    g.map( mapFunc );
+
+    g.printOut( );
+
+}
