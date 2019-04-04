ae11fbfb832bd04f2a669edaf1ce663cbc97c371
Eric Osburn <bjonesen@gmail.com>
2019-04-04 17:11:30 +0200

    RNN still not working as expected
    Former-commit-id: 2e4cdf3e7dbc05f48773527c42f7e019d09c823b

--- java/CodeMonkey/src/CodeMonkey/project/LoopingPillars.java
+++ java/CodeMonkey/src/CodeMonkey/project/LoopingPillars.java
@@ -16,10 +16,10 @@ public class LoopingPillars extends ProjectBase {
 
 	private Random rng = new Random( );
 
-	private int sWidth = 720;
+	private int sWidth = 360;
 	private int sHeigh = 320;
 
-	private int rnnLenH = this.sHeigh; // TODO: Vary this....
+	private int rnnLenH = this.sHeigh * 4;
 	private RNN gen;
 
 	int wCol = 1;
@@ -28,9 +28,26 @@ public class LoopingPillars extends ProjectBase {
 	@Override
 	public void settings( ) {
 
-		this.size( this.sWidth, this.sHeigh * 2 );
+		this.size( this.sWidth * 2, this.sHeigh * 2 );
 
 		this.setName( );
+
+	}
+
+	public void reset( ) {
+
+		this.gen.randWeight( this.rng );
+
+		for( int cdx = 0; cdx < this.sHeigh; ++cdx ) {
+			this.bufA[ cdx ] = this.rng.nextFloat( );
+			this.bufB[ cdx ] = 0;
+		}
+
+		this.gen.step( this.bufA, this.bufB );
+
+		for( int cdx = 0; cdx < this.sHeigh; ++cdx ) {
+			this.bufA[ cdx ] = 0;
+		}
 
 	}
 
@@ -44,15 +61,23 @@ public class LoopingPillars extends ProjectBase {
 		this.bufA = new float[ this.sHeigh ];
 		this.bufB = new float[ this.sHeigh ];
 
-		for( int cdx = 0; cdx < this.sHeigh; ++cdx )
-			this.bufA[ cdx ] = this.rng.nextFloat( );
+		this.reset( );
 
 	}
 
 	@Override
 	public void draw( ) {
 
+		// Input: A
+		// Output: B
 		this.gen.step( this.bufA, this.bufB );
+
+		// Swap A and B
+//		float[ ] temp = this.bufA;
+//		this.bufA = this.bufB;
+//		this.bufB = temp;
+
+		// A is now the latest output
 
 		// Get range ( softmax equiv? )
 		float m = Float.POSITIVE_INFINITY;
@@ -73,21 +98,25 @@ public class LoopingPillars extends ProjectBase {
 		M = 2;
 
 		// Write a column
-		this.loadPixels( );
+//		this.loadPixels( );
 		for( int ydx = 0; ydx < this.sHeigh; ++ydx ) {
 
-			this.pixels[ this.wCol + ( ydx * 2 ) * this.sWidth ] = this.color( ( this.bufB[ ydx ] - m ) / M * 255 );
-			this.pixels[ this.wCol + ( ydx * 2 + 1 ) * this.sWidth ] = this.color( ( this.bufB[ ydx ] - m ) / M * 255 );
+//			this.pixels[ this.wCol + ( ydx * 2 ) * this.sWidth ] = this.color( ( this.bufA[ ydx ] - m ) / M * 255 );
+//			this.pixels[ this.wCol + ( ydx * 2 + 1 ) * this.sWidth ] = this.color( ( this.bufA[ ydx ] - m ) / M * 255 );
+
+			this.noStroke( );
+			this.fill( ( this.bufB[ ydx ] - m ) / M * 255 );
+			this.rect( this.wCol, ydx * 2, this.wCol + 1, ydx * 2 + 1 );
 
 		}
-		this.updatePixels( );
+//		this.updatePixels( );
 
 		this.wCol = ( this.wCol + 1 ) % this.sWidth;
 
 		// ENTROPY
-		for( int ydx = 0; ydx < this.sHeigh; ++ydx )
-			if( this.rng.nextFloat( ) < 0.25 )
-				this.bufA[ ydx ] += this.rng.nextFloat( ) - 0.5;
+//		for( int ydx = 0; ydx < this.sHeigh; ++ydx )
+//			if( this.rng.nextFloat( ) < 0.25 )
+//				this.bufA[ ydx ] += this.rng.nextFloat( ) - 0.5;
 
 	}
 
@@ -96,10 +125,7 @@ public class LoopingPillars extends ProjectBase {
 
 		if( this.key == ' ' ) {
 
-			this.gen.randWeight( this.rng );
-
-			for( int cdx = 0; cdx < this.sHeigh; ++cdx )
-				this.bufA[ cdx ] = this.rng.nextFloat( );
+			this.reset( );
 
 		}
 
